a30903d1f0f204d62263a047ebef7edcf6299209
Daniel Barlow <dan@telent.net>
2012-01-21 17:57:05 +0000

    musing on permissions

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lectito::Application.config.secret_token = '41efce9bb320a43aad7491dabbfb2dd5dc4ff67cbab27f690369dea832119dbb0883283682c49a6bd53e9144bf798d0c2f65e4a526bc8fdad0f9cff07c33dde7'
