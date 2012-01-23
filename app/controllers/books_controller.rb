998774e36fe294e014b72221831dcfe69dcb797a
Daniel Barlow <dan@telent.net>
2012-01-23 21:51:35 +0000

    new book form

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,12 @@
 class BooksController < ApplicationController
   def index
   end
+  def new
+    @book=Book.new
+    @edition=Edition.new
+    @shelves=current_user.shelves
+    @collections=current_user.collections
+  end
+  def create
+  end
 end
