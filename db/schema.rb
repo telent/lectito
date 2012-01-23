818e44651ba42ff0087351a79111b123d4a811bc
Daniel Barlow <dan@telent.net>
2012-01-23 17:41:18 +0000

    stories table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120122181403) do
+ActiveRecord::Schema.define(:version => 20120123152256) do
 
   create_table "authorizations", :force => true do |t|
     t.string   "provider"
@@ -50,6 +50,18 @@ ActiveRecord::Schema.define(:version => 20120122181403) do
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
   end
+
+  create_table "stories", :force => true do |t|
+    t.string   "title"
+    t.string   "story"
+    t.integer  "user_id"
+    t.string   "url"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+    t.string   "link"
+  end
+
+  add_index "stories", ["user_id"], :name => "index_stories_on_user_id"
 
   create_table "users", :force => true do |t|
     t.string   "nickname"
