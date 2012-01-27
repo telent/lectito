28051d67afce7b07509a577e87152e1ce882210d
Daniel Barlow <dan@telent.net>
2012-01-27 13:11:20 +0000

    unwarn

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,7 +4,6 @@ CONF=YAML::load(ERB.new(IO.read(File.join(Rails.root,'config','omniauth.yml'))).
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   t=CONF["twitter"]
-  warn t
   provider :twitter, t["consumer_key"],t["consumer_secret"]
   f=CONF["facebook"]
   provider :facebook,f["app_id"], f["app_secret"]
