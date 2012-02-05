d48a93af18849011a4ba686a9903ab108cf4fc61
Daniel Barlow <dan@telent.net>
2012-02-05 18:53:04 +0000

    backbone book model gets methods for fetching associated objcts

--- app/assets/javascripts/backbone/models/book_methods.js
+++ app/assets/javascripts/backbone/models/book_methods.js
@@ -11,5 +11,30 @@
 		book.fetch(); 
 	    },
 	});
-    }
+    };
+    var async_get=function(coll,field) {
+	var id=this.get(field+"_id");
+	if(id) {
+	    var m= coll.fetchId(id);
+	    return (m.has("created_at")) ? m : undefined
+	} else {
+	    return null;
+	}
+    };
+   this.current_shelf=function() { 
+	return async_get.call(this,Store.shelves,"current_shelf") 
+    };
+    this.home_shelf=function() {
+	return async_get.call(this,Store.shelves,"home_shelf") 
+    };
+    this.book_collection=function() {  
+	return async_get.call(this,Store.collections,"collection") 
+    };
+    this.owner=function() {  
+	return async_get.call(this,Store.users,"owner") 
+    };
+    this.borrower=function() {
+	return async_get.call(this,Store.users,"borrower") 
+    };
+
 }).call(Lectito.Models.Book.prototype)
