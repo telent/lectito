cd844ab12a4c3ea2a67c7879fb2424144b526c7d
Daniel Barlow <dan@telent.net>
2012-02-05 14:06:28 +0000

    respond_with

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,14 @@
 class UsersController < ApplicationController
   def new
+    respond_with  @user=User.new
+  end
+  def index
+    respond_with @users=current_user.friends
   end
   def edit
   end
   def show
-    @user=User.find(params[:id])
+    respond_with @user=User.find(params[:id])
   end
 end
 
