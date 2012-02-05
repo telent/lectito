266337bd967c5c01f4b6c1f6ec9c595c7c2277f3
Daniel Barlow <dan@telent.net>
2012-02-05 18:51:18 +0000

    user has borrowed books

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,14 @@ class User < ActiveRecord::Base
   has_many :shelves,:order=>"name"
   has_many :collections
   has_many :books, :through => :collections
+  has_many :borrowed_books, :class_name=>"Book", :foreign_key=>:borrower_id
 
   def name
     "#{nickname} (#{fullname})"
+  end
+
+  def all_books
+    books + borrowed_books
   end
 
   def admin?
