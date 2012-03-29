b2f278827093b90b465c43877e803677007a7701
Daniel Barlow <dan@telent.net>
2012-03-29 15:15:03 +0100

    implement Membership#remove

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,4 +1,10 @@
 class Membership < ActiveRecord::Base
   belongs_to :user
   belongs_to :collection
+  default_scope  where(:removed_at=>nil)
+
+  def remove
+    self.removed_at=Time.now
+    self.save
+  end
 end
