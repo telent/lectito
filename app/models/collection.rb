0a0f0cd4414fb26c54ac8e602370a78aeb8a59e2
Daniel Barlow <dan@telent.net>
2012-03-29 13:36:03 +0100

    collection members are hm:t

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,8 +1,6 @@
 class Collection < ActiveRecord::Base
   belongs_to :user
   has_many :books
-  has_and_belongs_to_many :members, :join_table=>:collection_members,
-  :foreign_key=>:owner_id,
-  :association_foreign_key=>:member_id,
-  :class_name=>"User"
+  has_many :memberships
+  has_many :users, :through=>:memberships
 end
