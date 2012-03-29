0ca27083399a56ec67431e2cc923921109792dd5
Daniel Barlow <dan@telent.net>
2012-03-29 13:38:20 +0100

    users don't have members (at least, we are not *expecting* to need to model their limbs).  Collections have members

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,11 @@ class User < ActiveRecord::Base
   has_many :shelves,:order=>"name"
   has_many :collections
   has_many :books, :through => :collections
-  has_many :members, :through=>:collections
   has_many :borrowed_books, :class_name=>"Book", :foreign_key=>:borrower_id
   has_many :tags
 
+  # this bidirectional relationship appears impossible to do with 
+  # hm:t because it assumes the join table always has a primary key
   has_and_belongs_to_many :followers,:join_table=>:followlinks, 
   :class_name=>"User",
   :foreign_key=>:followed_id,
