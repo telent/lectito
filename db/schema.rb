aa0c8184d74ffa7730bffcaf870b8ad1fb7ca7a6
Daniel Barlow <dan@telent.net>
2012-03-29 15:19:20 +0100

    add removed_at to memberships

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120329121934) do
+ActiveRecord::Schema.define(:version => 20120329125948) do
 
   create_table "authorizations", :force => true do |t|
     t.string   "provider"
@@ -89,6 +89,7 @@ ActiveRecord::Schema.define(:version => 20120329121934) do
     t.integer  "collection_id"
     t.datetime "created_at",    :null => false
     t.datetime "updated_at",    :null => false
+    t.datetime "removed_at"
   end
 
   add_index "memberships", ["collection_id"], :name => "index_memberships_on_collection_id"
