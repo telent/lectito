5b49dcf9ee50ea3eecf305af16188f2952519eab
Daniel Barlow <dan@telent.net>
2012-06-01 12:42:47 +0100

    User had duplicate defns of follower/following

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,17 +6,6 @@ class User < ActiveRecord::Base
   has_many :borrowed_books, :class_name=>"Book", :foreign_key=>:borrower_id
   has_many :tags
 
-  # this bidirectional relationship appears impossible to do with 
-  # hm:t because it assumes the join table always has a primary key
-  has_and_belongs_to_many :followers,:join_table=>:followlinks, 
-  :class_name=>"User",
-  :foreign_key=>:followed_id,
-  :association_foreign_key=>:follower_id
-  has_and_belongs_to_many :following,:join_table=>:followlinks, 
-  :class_name=>"User",
-  :foreign_key=>:follower_id,
-  :association_foreign_key=>:followed_id
-
   has_many :following_rel, :foreign_key=>"follower_id",
   :class_name => "Relationship",:dependent=>:destroy
   has_many :follower_rel, :foreign_key=>"followed_id",
