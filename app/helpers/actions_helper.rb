a5f373a8173041656c159454e399291abb76dcb2
Daniel Barlow <dan@telent.net>
2012-06-01 18:48:58 +0100

    actions bar presents horizontal view of the same data as actions_dropdown is used for

--- app/helpers/actions_helper.rb
+++ app/helpers/actions_helper.rb
@@ -9,4 +9,13 @@ module ActionsHelper
       end
     end
   end
+  def actions_bar actions
+    content_tag(:div, class: 'actions_bar') do
+      content_tag(:ul) do
+        actions.map do |a| 
+          concat(content_tag(:li, a))
+        end
+      end
+    end
+  end
 end
