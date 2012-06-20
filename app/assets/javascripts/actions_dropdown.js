38e0d883871e520a4100cd4ed6d630bff74f2da3
Daniel Barlow <dan@telent.net>
2012-06-20 15:43:52 +0100

    actions menu jquery code needs to be a bit cleverer when rails ujs overrides link click handlers

--- app/assets/javascripts/actions_dropdown.js
+++ app/assets/javascripts/actions_dropdown.js
@@ -4,9 +4,19 @@ $(document).ready(function() {
 	$('span.actions_dropdown').removeClass('open');
 	if(!was) $(this).addClass('open');
     });
-    $('span.actions_dropdown li').click(function() {
-	var url=$('a',this)[0].href;
-	document.location=url;
-	return false;
+    $('span.actions_dropdown li').click(function(e,nested) {
+	// clicking anywhere in the LI should have same effect as 
+	// directly on the A.
+
+	// if the A element is subject to rails UJS (e.g. to change the 
+	// method from GET to POST) this is implemented by adding handlers
+	// for the click.rails to it, so we cannot simply visit its href
+	// attribute, we have to simulate the click
+
+	// we don't want that click to invoke this handler recursively 
+	// though, even though the A is contained withing this element
+	if(!nested) {
+	    $('a',this).trigger('click',[true]);
+	}
     });
 })
