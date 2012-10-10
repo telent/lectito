8afbd90480a51f328e6a7f673bf24e3af4364c9b
Daniel Barlow <dan@telent.net>
2012-10-10 21:42:13 +0100

    disallow users from messaging, blocking, or following themselves

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -25,16 +25,18 @@ class UserDecorator < ApplicationDecorator
     if user==me then
       a << h.link_to("Edit", h.edit_user_path(user))
     end
-    if me.following? user then
-      a << h.link_to("Message", h.user_path(user))
-      a << h.link_to("Unfollow", {action: :unfollow, id: self.id},{method: :post})
-    elsif !(me.blocking?(user) || user.blocking?(me))
-      a << h.link_to("Follow", {action: :follow, id: self.id},{method: :post})
-    end
-    if me.blocking? user then
-      a << h.link_to("Unblock", {action: :unblock, id: self.id},{method: :post})
-    else
-      a << h.link_to("Block",{action: :block, id: self.id},{method: :post})
+    unless (me==user) 
+      if me.following? user then
+        a << h.link_to("Message", h.user_path(user))
+        a << h.link_to("Unfollow", {action: :unfollow, id: self.id},{method: :post})
+      elsif !(me.blocking?(user) || user.blocking?(me))
+        a << h.link_to("Follow", {action: :follow, id: self.id},{method: :post})
+      end
+      if me.blocking? user then
+        a << h.link_to("Unblock", {action: :unblock, id: self.id},{method: :post})
+      else
+        a << h.link_to("Block",{action: :block, id: self.id},{method: :post})
+      end
     end
     a
   end
