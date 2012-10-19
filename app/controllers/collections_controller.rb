c17f845080913c00c9e09179109d6c0e27d65927
Daniel Barlow <dan@telent.net>
2012-10-19 22:25:07 +0100

    add sidebar to groups link

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,12 +1,11 @@
 class CollectionsController < ApplicationController
   def index
-    @collections=
-      if uid=params[:user_id] then
-        User.find(uid).collections
-      else
-        current_user.collections
-      end
-    respond_with @collections
+    @user = if uid=params[:user_id] then
+              User.find(uid)
+            else
+              current_user
+            end
+    respond_with @collections=@user.collections
   end
   def show
     respond_with @collection=Collection.find(params[:id])
