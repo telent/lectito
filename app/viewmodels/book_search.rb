3f9a1c6de5ac8ac2c727b1a769e49627a73a908c
Daniel Barlow <dan@telent.net>
2013-02-07 23:27:58 +0000

    BookSearch takes shelves, collections, start and end

--- app/viewmodels/book_search.rb
+++ app/viewmodels/book_search.rb
@@ -4,6 +4,11 @@ class BookSearch
     @class = a.delete(:class) || Book
     @start = a.delete(:start) || 0
     @end = (e=a.delete(:end)) ? (e-1) : nil
+    c_ids = (a.delete(:collection_ids) || []) +
+      (a.delete(:collections) || []).map(&:id)
+    s_ids = (a.delete(:shelf_ids) || []) +
+      (a.delete(:shelves) || []).map(&:id)
+    @where = {collection_ids: c_ids, shelf_ids: s_ids}
   end
   def results
     return @class.where(@where).offset(@start).limit(@end)
--- test/unit/book_search_test.rb
+++ test/unit/book_search_test.rb
@@ -5,14 +5,18 @@ MyBook ||= Class.new
 
 describe BookSearch do
   it "searches for books" do
-    mock(MyBook).where.with_any_args {
-      mock("result").offset.with(0) {
-        mock("result").limit.with(4) {
+    mock(MyBook).where(hash_including(collection_ids: [1,3],
+                                      shelf_ids: [7],
+                                      )) {
+      mock("result").offset(0) {
+        mock("result").limit(4) {
           Array.new(5,:book)
         }
       }
     }
-    r = BookSearch.new(:start=>0, :end=>5, :class=>MyBook)
+    r = BookSearch.new(:start=>0, :end=>5, :class=>MyBook,
+                       :collection_ids => [1,3],
+                       :shelves => [OpenStruct.new(id: 7 )])
     rows = r.results
   end
 end
