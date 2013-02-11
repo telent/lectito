d75dd0c196b7c6693e0c73f47eb80fc69d43caa8
Daniel Barlow <dan@telent.net>
2013-02-11 18:35:00 +0000

    book_search: don't search for collection_id in(null) if the caller did not specify collections

--- app/viewmodels/book_search.rb
+++ app/viewmodels/book_search.rb
@@ -4,11 +4,13 @@ class BookSearch
     @class = a.delete(:class) || Book
     @start = a.delete(:start) || 0
     @end = (e=a.delete(:end)) ? (e-1) : nil
+    @where = {}
     c_ids = (a.delete(:collection_ids) || []) +
       (a.delete(:collections) || []).map(&:id)
+    @where[:collection_id]= c_ids unless c_ids.empty?
     s_ids = (a.delete(:shelf_ids) || []) +
       (a.delete(:shelves) || []).map(&:id)
-    @where = {collection_id: c_ids, current_shelf_id: s_ids}
+    @where[:current_shelf_id] = s_ids
   end
   def results
     return @class.where(@where).offset(@start).limit(@end)
--- test/unit/book_search_test.rb
+++ test/unit/book_search_test.rb
@@ -16,4 +16,14 @@ describe BookSearch do
                        :shelves => [OpenStruct.new(id: 7 )])
     rows = r.results
   end
+  it "does not search by collection unless collections supplied" do
+    mock(MyBook).where(satisfy{|a| a[:current_shelf_id] && !a[:collection_id]}).
+      mock!.offset(0).mock!.limit(4) {
+      Array.new(5,:book)
+    }
+    r = BookSearch.new(:start=>0, :end=>5, :class=>MyBook,
+                       :shelves => [OpenStruct.new(id: 7 )])
+    rows = r.results
+    
+  end
 end
