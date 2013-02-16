b85fc957005bca3a027db141cb4e7749dc006e7c
Daniel Barlow <dan@telent.net>
2013-02-16 21:28:49 +0000

    Start work on the actual result format for BookSearch: if a book is owned by current user and is not on loan, 'location' will be its current shelf

--- app/viewmodels/book_search.rb
+++ app/viewmodels/book_search.rb
@@ -1,10 +1,11 @@
-class BookSearch
+class IdentityMappedBookSearch
   def initialize(args)
     a=args.dup
     @class = a.delete(:class) || Book
     @start = a.delete(:start) || 0
     @end = (e=a.delete(:end)) ? (e-1) : nil
     @where = {}
+    @user = a.delete(:user)
     c_ids = (a.delete(:collection_ids) || []) +
       (a.delete(:collections) || []).map(&:id)
     @where[:collection_id] = c_ids unless c_ids.empty?
@@ -12,7 +13,28 @@ class BookSearch
       (a.delete(:shelves) || []).map(&:id)
     @where[:current_shelf_id] = s_ids unless s_ids.empty?
   end
-  def results
-    return @class.where(@where).offset(@start).limit(@end)
+
+  def row_mapper book
+    book
   end
+
+  def results
+    results = @class.where(@where).offset(@start).limit(@end)
+    results.map {|r| row_mapper(r) }
+  end
+
+end
+
+class BookSearch < IdentityMappedBookSearch
+  def row_mapper book
+    loc = case 
+          when book.owned_by?(@user) && !book.on_loan? then
+            book.current_shelf.name
+          else "sdfgsdfg"
+          end
+    { 
+      location: loc
+    }
+  end
+
 end
--- test/unit/book_search_test.rb
+++ test/unit/book_search_test.rb
@@ -4,37 +4,64 @@ require 'viewmodels/book_search'
 MyBook ||= Class.new
 
 describe BookSearch do
+  let :book do
+    OpenStruct.new(title: "Hello World",
+                   current_shelf: OpenStruct.new(name: 'my shelf'))
+  end
   it "searches for books" do
     mock(MyBook).where(hash_including(collection_id: [1,3],
                                       current_shelf_id: [7],
                                       )).mock!.
       offset(0).mock!.limit(4) {
-      Array.new(5,:book)
+      Array.new(5,book)
     }
-    r = BookSearch.new(:start=>0, :end=>5, :class=>MyBook,
-                       :collection_ids => [1,3],
-                       :shelves => [OpenStruct.new(id: 7 )])
+    r = IdentityMappedBookSearch.new(:start=>0, :end=>5, :class=>MyBook,
+                                     :collection_ids => [1,3],
+                                     :shelves => [OpenStruct.new(id: 7 )])
     rows = r.results
   end
+
   it "does not search by collection unless collections supplied" do
     mock(MyBook).where(satisfy{|a| a[:current_shelf_id] && !a[:collection_id]}).
       mock!.offset(0).mock!.limit(4) {
-      Array.new(5,:book)
+      Array.new(5,book)
     }
-    r = BookSearch.new(:start=>0, :end=>5, :class=>MyBook,
-                       :shelves => [OpenStruct.new(id: 7 )])
+    r = IdentityMappedBookSearch.new(:start=>0, :end=>5, :class=>MyBook,
+                                     :shelves => [OpenStruct.new(id: 7 )])
     rows = r.results
-    
   end
+
   it "does not search by shelf unless shelves supplied" do
     mock(MyBook).where(satisfy{|a| !a[:current_shelf_id] && a[:collection_id]}).
       mock!.offset(0).mock!.limit(4) {
-      Array.new(5,:book)
+      Array.new(5,book)
     }
-    r = BookSearch.new(:start=>0, :end=>5, :class=>MyBook,
-                       :collection_ids => [1,3]
-                       )
+    r = IdentityMappedBookSearch.new(:start=>0, :end=>5, :class=>MyBook,
+                                     :collection_ids => [1,3]
+                                     )
     rows = r.results
-    
+  end
+  
+  describe "#location" do
+    before do
+      stub(MyBook).where.stub!.offset.stub!.limit {
+        [book]
+      }
+    end
+    describe "book belongs to me" do
+      # now our viewmodel needs to get the current user
+      describe "book is at home" do
+        it "returns shelf name" do
+          user = "a user"
+          stub(book).on_loan? { false }
+          stub(book).owned_by? { false }
+          stub(book).owned_by?(user) { true }
+          r = BookSearch.new(:start=>0, :end=>5, :class=>MyBook,
+                             :user=>user)
+          row = r.results.first
+          assert_equal 'my shelf', row[:location]
+        end
+      end
+    end
   end
 end
